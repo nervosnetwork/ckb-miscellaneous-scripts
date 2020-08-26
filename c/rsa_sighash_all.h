6eba2beb84f3c81af7ee41324d9a1c582a2ccee4
xjd <xjd@cryptape.com>
2020-08-26 16:09:18 +0800

    fix clang-format -i -style=Google

--- c/rsa_sighash_all.h
+++ c/rsa_sighash_all.h
@@ -5,8 +5,9 @@
 #ifndef CKB_MISCELLANEOUS_SCRIPTS_RSA_SIGHASH_ALL_H
 #define CKB_MISCELLANEOUS_SCRIPTS_RSA_SIGHASH_ALL_H
 
+#include <string.h>
 
-#include <stdint.h>
+#include "mbedtls/rsa.h"
 
 /**
  * This structure contains the following information:
@@ -14,34 +15,34 @@
  * 2) RSA Public Key
  * 3) Real Signature data
  *
- * Because we need to use the same interfaces (see validate_signature) as secp256k1,
- * store 1) and 2) information alone with signature.
+ * Because we need to use the same interfaces (see validate_signature) as
+ * secp256k1, store 1) and 2) information alone with signature.
  */
 typedef struct RsaInfo {
-    // RSA Key Size, in bits. For example, 1024, 2048.
-    // Normally we use 1024; Choose 2048 for safety.
-    uint16_t key_size;
+  // RSA Key Size, in bits. For example, 1024, 2048.
+  // Normally we use 1024; Choose 2048 for safety.
+  uint16_t key_size;
 
-    // RSA public key, part E. It's normally very small, OK to use uint32_to hold it.
-    // https://eprint.iacr.org/2008/510.pdf
-    // The choice e = 65537 = 2^16 + 1 is especially widespread. Of the certificates observed in the UCSD TLS
-    // Corpus [23] (which was obtained by surveying frequently-used TLS servers), 99.5% had e = 65537,
-    // and all had e at most 32 bits.
-    uint32_t E;
+  // RSA public key, part E. It's normally very small, OK to use uint32_to hold
+  // it. https://eprint.iacr.org/2008/510.pdf The choice e = 65537 = 2^16 + 1 is
+  // especially widespread. Of the certificates observed in the UCSD TLS Corpus
+  // [23] (which was obtained by surveying frequently-used TLS servers), 99.5%
+  // had e = 65537, and all had e at most 32 bits.
+  uint32_t E;
 
-    // RSA public key, part N.
-    // The public key is the combination of E and N.
-    // But N is a very large number and need to use array to represent it.
-    // The total length in byte is key_size/8 (The key_size is in bits).
-    // The memory layout is the same as the field "p" of mbedtls_mpi type.
-    uint8_t* N;
+  // RSA public key, part N.
+  // The public key is the combination of E and N.
+  // But N is a very large number and need to use array to represent it.
+  // The total length in byte is key_size/8 (The key_size is in bits).
+  // The memory layout is the same as the field "p" of mbedtls_mpi type.
+  uint8_t *N;
 
-    // length of signature, in bytes.
-    uint32_t sig_length;
-    // pointer to signature
-    uint8_t* sig;
+  // length of signature, in bytes.
+  uint32_t sig_length;
+  // pointer to signature
+  uint8_t *sig;
 } RsaInfo;
 
-int md_string(const mbedtls_md_info_t *md_info, const unsigned  char *buf, size_t n, unsigned char *output);
-void mbedtls_mpi_dump(const char *prefix, const mbedtls_mpi *X);
-#endif //CKB_MISCELLANEOUS_SCRIPTS_RSA_SIGHASH_ALL_H
+int md_string(const mbedtls_md_info_t *md_info, const unsigned char *buf,
+              size_t n, unsigned char *output);
+#endif  // CKB_MISCELLANEOUS_SCRIPTS_RSA_SIGHASH_ALL_H
