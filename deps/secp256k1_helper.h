67513ac33dfd6fca526a2b17b0af1b8a9f2653da
jjy <jjyruby@gmail.com>
2021-01-12 23:09:52 +0800

    fix: secp256k1 recovery invalid pubkey

--- deps/secp256k1_helper.h
+++ deps/secp256k1_helper.h
@@ -29,35 +29,53 @@ void secp256k1_default_error_callback_fn(const char* str, void* data) {
   ckb_exit(CKB_SECP256K1_HELPER_ERROR_ERROR_CALLBACK);
 }
 
-int ckb_secp256k1_custom_load_data(void* data) {
-  size_t index = SIZE_MAX;
-  int ret = ckb_look_for_dep_with_hash(ckb_secp256k1_data_hash, &index);
-  if (ret != CKB_SUCCESS) {
-    return ret;
-  }
-  /* Found a match, load data here */
-  uint64_t len = CKB_SECP256K1_DATA_SIZE;
-  ret = ckb_load_cell_data(data, &len, 0, index, CKB_SOURCE_CELL_DEP);
-  if (ret != CKB_SUCCESS || len != CKB_SECP256K1_DATA_SIZE) {
-    return CKB_SECP256K1_HELPER_ERROR_LOADING_DATA;
-  }
-  return CKB_SUCCESS;
-}
-
 /*
  * data should at least be CKB_SECP256K1_DATA_SIZE big
  * so as to hold all loaded data.
  */
 int ckb_secp256k1_custom_verify_only_initialize(secp256k1_context* context,
-                                                void* loaded_data) {
+                                                void* data) {
+  size_t index = 0;
+  int running = 1;
+  while (running && index < SIZE_MAX) {
+    uint64_t len = 32;
+    uint8_t hash[32];
+
+    int ret = ckb_load_cell_by_field(hash, &len, 0, index, CKB_SOURCE_CELL_DEP,
+                                     CKB_CELL_FIELD_DATA_HASH);
+    switch (ret) {
+      case CKB_ITEM_MISSING:
+        break;
+      case CKB_SUCCESS:
+        if (memcmp(ckb_secp256k1_data_hash, hash, 32) == 0) {
+          /* Found a match, load data here */
+          len = CKB_SECP256K1_DATA_SIZE;
+          ret = ckb_load_cell_data(data, &len, 0, index, CKB_SOURCE_CELL_DEP);
+          if (ret != CKB_SUCCESS || len != CKB_SECP256K1_DATA_SIZE) {
+            return CKB_SECP256K1_HELPER_ERROR_LOADING_DATA;
+          }
+          running = 0;
+        }
+        break;
+      default:
+        return CKB_SECP256K1_HELPER_ERROR_LOADING_DATA;
+    }
+    if (running) {
+      index++;
+    }
+  }
+  if (index == SIZE_MAX) {
+    return CKB_SECP256K1_HELPER_ERROR_LOADING_DATA;
+  }
+
   context->illegal_callback = default_illegal_callback;
   context->error_callback = default_error_callback;
 
   secp256k1_ecmult_context_init(&context->ecmult_ctx);
   secp256k1_ecmult_gen_context_init(&context->ecmult_gen_ctx);
 
-  /* Recasting loaded_data to (uint8_t*) for pointer math */
-  uint8_t* p = loaded_data;
+  /* Recasting data to (uint8_t*) for pointer math */
+  uint8_t* p = data;
   secp256k1_ge_storage(*pre_g)[] = (secp256k1_ge_storage(*)[])p;
   secp256k1_ge_storage(*pre_g_128)[] =
       (secp256k1_ge_storage(*)[])(&p[CKB_SECP256K1_DATA_PRE_SIZE]);
